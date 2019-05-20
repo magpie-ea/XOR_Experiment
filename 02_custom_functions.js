145ebb1e029d307ba1c6448245e0b922a0746040
MalinSpaniol <mspaniol@uos.de>
2019-05-20 09:36:18 +0200

    new version with latest babe version 1.0

---
+++ 02_custom_functions.js
@@ -0,0 +1,169 @@
+// Here, you can define all custom functions, you want to use and initialize some variables
+
+// add additional attribute to obects vignettes
+function add_complete_test_question (vign){
+  console.log("malins function wird aufgerufen");
+  let test;
+  for (var i = 0; i < vign.length; i++) {
+    console.log(vign[i]);
+    vign[i].allQ = [vign[i].test_true1, vign[i].test_true2, vign[i].test_false1, vign[i].test_false2, vign[i].test_uncertain1, vign[i].test_uncertain2];
+    //let all_test_questions = {vign[i].test_true1, vign[i].test_true2, vign[i].test_false1, vign[i].test_false2, vign[i].test_uncertain1, vign[i].test_uncertain2}
+      // console.log(all_test_questions);
+  }
+console.log(vign);
+
+    return (vign);
+}
+
+
+
+// create competence block
+function create_comp_block(a) {
+          var b = a;
+          b.title = b.name;
+          b.QUD = '<font size="6">'+ b.title + '</font>  <br /> <br />' + b.background;
+          b.optionLeft = "certainly false";
+          b.optionRight  = "certainly true";
+          b.question =   `------------------------------- <br/> <font size="2">
+          How likely do you think it is that the statement is true, given the
+          information in the background story?</font> <br/>
+          ------------------------------- <br/>`
+          + b.question_comp;
+          b.test_true1 = b.test_true1;
+          return(b);
+}
+
+// creating new prior 1 block
+function create_prior1_block(a) {
+      var b = a;
+      b.title = b.name;
+      b.QUD = '<font size="6">'+ b.title + '</font>  <br /> <br />' + b.background;
+      b.optionLeft = "certainly false";
+      b.optionRight  = "certainly true";
+      b.question = `How likely do you think it is that the statement is true, given the information in the background story? <br/> <br/> ------------------------------- <br/>`  +b.question_pri1;
+      b.test_true1 = b.test_true1;
+      return(b);
+}
+
+// creating prior two block
+function create_prior2_block(a) {
+         var b = a;
+         b.title = b.name;
+         b.QUD = '<font size="6">'+ b.title + '</font>  <br /> <br />' + b.background;
+         b.optionLeft = "certainly false";
+         b.optionRight  = "certainly true";
+         b.question = `How likely do you think it is that the statement is true, given the information in the background story? <br/> <br/> ------------------------------- <br/>`  +b.question_pri2;
+         b.test_true1 = b.test_true1;
+         return(b);
+}
+
+// create relevance block
+function create_rel_block(a) {
+         var b = a;
+         b.title = b.name;
+         b.QUD = '<font size="6">'+ b.title + '</font>  <br /> <br />' + b.background;
+         b.optionLeft = "certainly false";
+         b.optionRight  = "certainly true";
+         b.question = `How likely do you think it is that the statement is true,
+          given the information in the background story? <br/> <br/>
+          ------------------------------- <br/>`  +
+          b.question_rel;
+         b.test_true1 = b.test_true1;
+         return(b);
+}
+
+
+
+//creating new xor block-based trial info
+function create_xor_block(a) {
+      var b = a;
+      b.title = b.name;
+      b.QUD = '<font size="6">'+ b.title + '</font>  <br /> <br />' + b.background;
+      b.optionLeft = "certainly false";
+      b.optionRight  = "certainly true";
+      b.question = `------------------------------- <br/> <font size="2"> How likely do you think it is that the statement is true, given the information in the background story?</font> <br/> ------------------------------- <br/>`  +b.question_xor;
+      b.test_true1 = b.test_true1;
+      return(b);
+}
+
+
+// function to create hook to show second question in same view
+// only for competence
+
+//function show2ndquestion(data,next){
+//  comp_block.test_true1
+//}
+
+
+
+
+
+
+/* Variables
+*
+*
+*/
+const coin = _.sample(["head", "tail"]); // You can determine global (random) parameters here
+// Declare your variables here
+
+
+
+/* Helper functions
+*
+*
+*/
+
+
+/* For generating random participant IDs */
+    // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+// dec2hex :: Integer -> String
+const dec2hex = function(dec) {
+    return ("0" + dec.toString(16)).substr(-2);
+};
+// generateId :: Integer -> String
+const generateID = function(len) {
+    let arr = new Uint8Array((len || 40) /2);
+    window.crypto.getRandomValues(arr);
+    return Array.from(arr, this.dec2hex).join("");
+};
+// Declare your helper functions here
+
+
+
+/* Hooks
+*
+*
+*/
+
+// Error feedback if participants exceeds the time for responding
+const time_limit = function(data, next) {
+    if (typeof window.timeout === 'undefined'){
+        window.timeout = [];
+    }
+    // Add timeouts to the timeoutarray
+    // Reminds the participant to respond after 5 seconds
+    window.timeout.push(setTimeout(function(){
+          $('#reminder').text('Please answer more quickly!');
+    }, 5000));
+    next();
+};
+
+// compares the chosen answer to the value of `option1`
+check_response = function(data, next) {
+    $('input[name=answer]').on('change', function(e) {
+        if (e.target.value === data.correct) {
+            alert('Your answer is correct! Yey!');
+        } else {
+            alert('Sorry, this answer is incorrect :( The correct answer was ' + data.correct);
+        }
+        next();
+    })
+}
+
+// Declare your hooks here
+
+
+/* Generators for custom view templates, answer container elements and enable response functions
+*
+*
+*/
---
+++ 05_views.js
@@ -0,0 +1,193 @@
+// In this file you can instantiate your views
+// We here first instantiate wrapping views, then the trial views
+
+
+/** Wrapping views below
+
+* Obligatory properties
+
+    * trials: int - the number of trials this view will appear
+    * name: string
+
+*Optional properties
+    * buttonText: string - the text on the button (default: 'next')
+    * text: string - the text to be displayed in this view
+    * title: string - the title of this view
+
+    * More about the properties and functions of the wrapping views - https://babe-project.github.io/babe-docs/01_designing_experiments/01_template_views/#wrapping-views
+
+*/
+
+// Every experiment should start with an intro view. Here you can welcome your participants and tell them what the experiment is about
+const intro = babeViews.view_generator("intro",{
+    trials: 1,
+    name: 'intro',
+    // If you use JavaScripts Template String `I am a Template String`, you can use HTML <></> and javascript ${} inside
+    text:  `Welcome to an XPrag Experiment
+            <br />
+            <br />
+            Thank you for your participation in our study!
+            Your anonymous data makes an important contribution to our understanding of human learning and memory.
+            <br />
+            <br />
+            Legal information:
+            By answering the following questions, you are participating in a study
+            being performed by scientists from the University of Osnabrueck.
+            <br />
+            <br />
+            You must be at least 18 years old to participate.
+            <br />
+            <br />
+            Your participation in this research is voluntary.
+            You may decline to answer any or all of the following questions.
+            You may decline further participation, at any time, without adverse consequences.
+            <br />
+            <br />
+            Your anonymity is assured; the researchers who have requested your
+            participation will not receive any personal information about you.`,
+   buttonText: 'Begin the experiment'
+});
+
+// For most tasks, you need instructions views
+const instructions = babeViews.view_generator("instructions",{
+    trials: 1,
+    name: 'instrucions',
+    title: 'General Instructions',
+    text:  `In the following, you will be presented with 8 short stories.
+            Please read them very carefully, even if they appear to be repeated and you think that you remember them well enough.
+            We ask you to rate statements about each short story.
+            Please indicate, using an adjustable slider bar, how likely you think a statement
+            is true based on the story you read.
+            <br />
+            <br />
+            The experiment consists of 4 blocks with pauses between.
+            Within each block you are asked to rate different kinds of statements.
+            <br />
+            <br />
+            The background story is in a grey box.
+            The statements to be rated are preceded by ?
+            <br />
+            <br />
+            An example is provided next.`,
+    buttonText: 'next'
+});
+
+
+// In the post test questionnaire you can ask your participants addtional questions
+const post_test = babeViews.view_generator("post_test",{
+    trials: 1,
+    name: 'post_test',
+    title: 'Additional information',
+    text: 'Answering the following questions is optional, but your answers will help us analyze our results.'
+
+    // You can change much of what appears here, e.g., to present it in a different language, as follows:
+    // buttonText: 'Weiter',
+    // age_question: 'Alter',
+    // gender_question: 'Geschlecht',
+    // gender_male: 'männlich',
+    // gender_female: 'weiblich',
+    // gender_other: 'divers',
+    // edu_question: 'Höchster Bildungsabschluss',
+    // edu_graduated_high_school: 'Abitur',
+    // edu_graduated_college: 'Hochschulabschluss',
+    // edu_higher_degree: 'Universitärer Abschluss',
+    // languages_question: 'Muttersprache',
+    // languages_more: '(in der Regel die Sprache, die Sie als Kind zu Hause gesprochen haben)',
+    // comments_question: 'Weitere Kommentare'
+});
+
+// The 'thanks' view is crucial; never delete it; it submits the results!
+const thanks = babeViews.view_generator("thanks", {
+    trials: 1,
+    name: 'thanks',
+    title: 'Thank you for taking part in this experiment!',
+    prolificConfirmText: 'Press the button'
+});
+
+
+//my own objects
+
+const comp_trial = babeViews.view_generator("slider_rating",{
+  trials: 1,
+  name: 'comp_question',
+  trial_type:'comp_slider',
+  data: _.shuffle(comp_block),
+  //hook: {
+  //      after_response_enabled: show2ndquestion
+  //    }
+
+})
+
+const pri_trial = babeViews.view_generator("slider_rating", {
+    trials:1, //xor_trial_block.slider_rating.length,
+    //title: pri1_block.title,
+    name: 'prior_probability_question',
+    trial_type: 'prior_slider',
+    data: _.shuffle(pri2_block, pri1_block) //pri2_block
+});
+
+
+const rel_trial = babeViews.view_generator("slider_rating",{
+  trials: 1,
+  name: 'rel_question',
+  trial_type:'comp_slider',
+  data: _.shuffle(rel_block),
+  //hook: {
+  //      after_response_enabled: show2ndquestion
+  //    }
+
+})
+
+const xor_trial = babeViews.view_generator("slider_rating", {
+    trials:1, //xor_trial_block.slider_rating.length,
+    //title: xor_block.title,
+    name: 'xor_question',
+    trial_type: 'xor_slider',
+    data: _.shuffle(xor_block)
+});
+
+
+
+
+
+
+/** trial (babe's Trial Type Views) below
+
+* Obligatory properties
+
+    - trials: int - the number of trials this view will appear
+    - name: string - the name of the view type as it shall be known to _babe (e.g. for use with a progress bar)
+            and the name of the trial as you want it to appear in the submitted data
+    - data: array - an array of trial objects
+
+* Optional properties
+
+    - pause: number (in ms) - blank screen before the fixation point or stimulus show
+    - fix_duration: number (in ms) - blank screen with fixation point in the middle
+    - stim_duration: number (in ms) - for how long to have the stimulus on the screen
+      More about trial life cycle - https://babe-project.github.io/babe-docs/01_designing_experiments/04_lifecycles_hooks/
+
+    - hook: object - option to hook and add custom functions to the view
+      More about hooks - https://babe-project.github.io/babe-docs/01_designing_experiments/04_lifecycles_hooks/
+
+* All about the properties of trial views
+* https://babe-project.github.io/babe-docs/01_designing_experiments/01_template_views/#trial-views
+*/
+
+
+// Here, we initialize a normal forced_choice view
+const forced_choice_2A = babeViews.view_generator("forced_choice", {
+    // This will use all trials specified in `data`, you can user a smaller value (for testing), but not a larger value
+    trials: trial_info.forced_choice.length,
+    // name should be identical to the variable name
+    name: 'forced_choice_2A',
+    data: trial_info.forced_choice,
+    // you can add custom functions at different stages through a view's life cycle
+    // hook: {
+    //     after_response_enabled: check_response
+    // }
+});
+
+// There are many more templates available:
+// forced_choice, slider_rating, dropdown_choice, testbox_input, rating_scale, image_selection, sentence_choice,
+// key_press, self_paced_reading and self_paced_reading_rating_scale
