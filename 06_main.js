11d88d99345ead06a0ca6244c5104aaa41d52380
polina-tsvilodub <polina.tsvilodub@gmail.com>
2021-03-24 10:00:36 +0100

    add prolific url

--- 06_main.js
+++ 06_main.js
@@ -37,7 +37,7 @@ $("document")
         // As well as "MTurk", "MTurkSandbox" and "Prolific"
         deployMethod: "Prolific",
         contact_email: "polina.tsvilodub@gmail.com",
-        prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
+        prolificURL: "https://app.prolific.co/submissions/complete?cc=135C5AFF"
       },
       // Here, you can specify how the progress bar should look like
       progress_bar: {
